9611efd1e2ebd1b313a66bf9626d5980154f7db3
Arildo Magno <macedoarildo@hotmail.com>
2022-05-02 14:35:36 -0300

    Portando codigo original

--- firstproject/interface/views.py
+++ firstproject/interface/views.py
@@ -6,8 +6,10 @@ from django.utils import timezone
 from django.shortcuts import render
 from .models import Interface, File
 from django.core.files.storage import FileSystemStorage
-
 import os
+
+from interface.textdata import TextManipulation
+from interface.similarity import Similarity
 
 
 class IndexView(generic.ListView):
@@ -65,24 +67,49 @@ def func2():
 
 # CONSEGUI ENVIAR OS ARQUIVOS TXT
 def upload(request):
+    # Aí se ele receber uma requisicao tipo POST faz o metodo
+    # caso contrario apenas uma GET nao executa
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         uploaded_file2 = request.FILES['document2']
-        print("File1:", uploaded_file.name)
-        print(uploaded_file.size)
-        print("File2:", uploaded_file2.name)
-        print(uploaded_file2.size)
-
+        doc1_name = uploaded_file.name
+        doc2_name = uploaded_file2.name
+        doc1 = ""
+        doc2 = ""
         for i in uploaded_file:
-            print(i)
-            test_string = i.decode("utf-8")
-            print(test_string)
+            doc1 = i.decode("utf-8")
 
         for j in uploaded_file2:
-            print(j)
-            test_string = j.decode("utf-8")
-            print(test_string)
+            doc2 = j.decode("utf-8")
 
-        # fs = FileSystemStorage()
-        # fs.save(uploaded_file.name, uploaded_file)
+        print(analyse_docs(doc1_name, doc2_name, doc1, doc2))
+
+    # fs = FileSystemStorage()
+    # fs.save(uploaded_file.name, uploaded_file)
     return render(request, 'interface\\upload.html')
+    # return HttpResponse("OK")
+
+
+# REPLICANDO AS FUNÇÕES DE MANEIRA FEIA E NAO MODULARIZADA:
+
+
+def analyse_docs(file_name1, file_name2, doc1, doc2):
+    print("\nAnalisando docs:", file_name1, file_name2)
+    text_manipulation = TextManipulation()
+    similarity = Similarity()
+
+    # # FEM REPLICA ESSE ALGORITMO DE LA AQUI
+    doc1_segmented = text_manipulation.segmentation_based_sentences(doc1)
+    doc2_segmented = text_manipulation.segmentation_based_sentences(doc2)
+
+    # #  SIMILARITY 1: (doc1 em relação ao doc2) SÓ REPLICAR O METODO AQUI:
+    qntd_similar_sets1, similar_sets_log1 = similarity.calculate_similar_sets_in_docs(doc1_segmented, doc2_segmented)
+    degree_resemblance1 = similarity.degree_resemblance(qntd_similar_sets1, len(doc1_segmented))
+
+    #  SIMILARITY 2: (doc2 em relação ao doc1)
+    qntd_similar_sets2, similar_sets_log2 = similarity.calculate_similar_sets_in_docs(doc2_segmented, doc1_segmented)
+    degree_resemblance2 = similarity.degree_resemblance(qntd_similar_sets2, len(doc2_segmented))
+
+    percent_plagiarism = similarity.odds_ratio_in_percent(degree_resemblance1, degree_resemblance2)
+
+    return (file_name1, file_name2, similar_sets_log1, similar_sets_log2, percent_plagiarism)
